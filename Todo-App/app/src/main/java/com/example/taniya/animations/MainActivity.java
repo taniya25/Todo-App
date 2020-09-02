c1e30df9e072851697c2321c9ff9172eb2e0627f
Taniya <tanu2599@gmail.com>
2020-09-02 20:11:51 +0530

    Added changes

--- Todo-App/app/src/main/java/com/example/taniya/animations/MainActivity.java
+++ Todo-App/app/src/main/java/com/example/taniya/animations/MainActivity.java
@@ -3,12 +3,14 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
