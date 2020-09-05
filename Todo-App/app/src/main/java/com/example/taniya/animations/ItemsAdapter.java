58c93e9eaa65fcc5a4ab6d0851baba6ee8f40c32
Taniya <tanu2599@gmail.com>
2020-09-05 13:27:38 +0530

    feat(app): Basic app

---
+++ Todo-App/app/src/main/java/com/example/taniya/animations/ItemsAdapter.java
@@ -0,0 +1,63 @@
+package com.example.taniya.animations;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+    List<String> items;
+    private ViewHolder holder;
+    private int position;
+
+    public ItemsAdapter(ArrayList<String> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Use layout inflator to inflate a view
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        // wrap it inside a View holder and return it
+        return new ViewHolder(todoView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //taking data at a particular position and putting it in our view holder
+        //Grab item at the position
+        String item = items.get(position);
+
+        //Bind item into the specified VH
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        //number of items available in our data
+        return items.size();
+    }
+
+    // Container to provide easy access to views that represent each row of the list
+    class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvItem;
+        public ViewHolder(View itemView){
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        //Update the view inside of the VH with this data
+        public void bind(String item) {
+            tvItem.setText(item);
+
+        }
+    }
+
+}
--- Todo-App/app/src/main/java/com/example/taniya/animations/MainActivity.java
+++ Todo-App/app/src/main/java/com/example/taniya/animations/MainActivity.java
@@ -1,16 +1,39 @@
  package com.example.taniya.animations;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
  public class MainActivity extends AppCompatActivity {
+     ArrayList<String> items;
+     Button button;
+     EditText item;
+     RecyclerView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        button = findViewById(R.id.button);
+        item = findViewById(R.id.addItem);
+        list = findViewById(R.id.listView);
+
+        items = new ArrayList<>();
+        items.add("Buy Milk");
+        items.add("Go to the Gym");
+        items.add("Have fun");
+
+        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        list.setAdapter(itemsAdapter);
+        list.setLayoutManager(new LinearLayoutManager(this));
+    }
+    
 }
